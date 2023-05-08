c14d63d3fc51b304272aabd962da6e205d059a81
Zach Manson <zachpmanson@gmail.com>
2023-05-08 19:16:40 +0800

    Add handling for gods mistakes

--- src/pages/tab/[...id].tsx
+++ src/pages/tab/[...id].tsx
@@ -50,7 +50,6 @@ export default function Tab({ tabDetails }: TabProps) {
 
   const plainTab = tabDetails.tab;
   const tabLink = convertToTabLink(tabDetails);
-
   useEffect(() => {
     const recents: any = JSON.parse(localStorage?.getItem("recents") || "{}");
     if (Array.isArray(recents)) {
@@ -126,7 +125,7 @@ export default function Tab({ tabDetails }: TabProps) {
   useEffect(() => {
     const onTouch = () => (isTouching.current = true);
     const onTouchEnd = () =>
-      setTimeout(() => (isTouching.current = false), 1000);
+      setTimeout(() => (isTouching.current = false), 2000);
     window.addEventListener("touchstart", onTouch);
     window.addEventListener("touchend", onTouchEnd);
 
