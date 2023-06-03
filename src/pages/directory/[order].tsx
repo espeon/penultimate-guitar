6c29036029cc36b7950493f578822303aa536932
Zach Manson <zachpmanson@gmail.com>
2023-06-03 19:38:31 +0800

    Add song group on dir page

--- src/pages/directory/[order].tsx
+++ src/pages/directory/[order].tsx
@@ -5,101 +5,130 @@ import prisma from "@/lib/prisma";
 import { TabDto } from "@/models/models";
 import { Song } from "@prisma/client";
 import _ from "lodash";
+import { GetStaticProps } from "next";
 
 import Head from "next/head";
 import Link from "next/link";
-import router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+type TabMetadata = {
+  songId: number;
+  taburl: string;
+  type: string;
+  version: number;
+  song: Song;
+  timestamp: string;
+};
 
 type ListProps = {
-  allTabs: {
-    songId: number;
-    taburl: string;
-    type: string;
-    version: number;
-    song: Song;
-    timestamp: string;
-  }[];
+  allTabs: TabMetadata[];
 };
 
 export default function Directory({ allTabs }: ListProps) {
-  const { searchText, savedTabs } = useGlobal();
-  const lowerSearchText = searchText.toLowerCase();
-  const filteredTabs = _.uniqBy(
-    allTabs.filter(
-      (t) =>
-        t.song.name.toLowerCase().includes(lowerSearchText) ||
-        t.song.artist.toLowerCase().includes(lowerSearchText)
-    ),
-    (t: {
-      songId: number;
-      taburl: string;
-      type: string;
-      version: number;
-      song: Song;
-      timestamp: string;
-    }) => t.taburl
-  );
+  const router = useRouter();
+  const [collapseVersions, setCollapseVersions] = useState(false);
 
-  const multipleVersions: { [key: string]: boolean } = {};
-  for (let tab of filteredTabs) {
-    if (multipleVersions[tab.songId] === undefined) {
-      multipleVersions[tab.songId] = false;
-    } else if (multipleVersions[tab.songId] === false) {
-      multipleVersions[tab.songId] = true;
+  const groupedOrder: number[] = [];
+  const groupedVersions: { [key: string]: TabMetadata[] } = {};
+  for (let tab of allTabs) {
+    if (groupedVersions[tab.songId] === undefined) {
+      groupedVersions[tab.songId] = [tab];
+      groupedOrder.push(tab.songId);
+    } else {
+      groupedVersions[tab.songId].push(tab);
     }
   }
 
-  console.log(savedTabs)
+  const generateLink = (t: TabMetadata) => (
+    <Link href={`/tab/${t.taburl}`} prefetch={false} title={t.timestamp}>
+      {t.song.artist} - {t.song.name}
+      {groupedVersions[t.songId].length > 1 && (
+        <span className="font-light text-xs">
+          {" "}
+          ({t.type}) (v{t.version})
+        </span>
+      )}
+    </Link>
+  );
+
+  const tabList = collapseVersions ? (
+    <>
+      {groupedOrder.map((songId, i) => (
+        <li key={i}>
+          {groupedVersions[songId].length === 1 ? (
+            generateLink(groupedVersions[songId][0])
+          ) : (
+            <details>
+              <summary>
+                {groupedVersions[songId][0].song.artist} -{" "}
+                {groupedVersions[songId][0].song.name}{" "}
+                <span className="font-light text-xs">
+                  ({groupedVersions[songId].length} versions)
+                </span>
+              </summary>
+              <ul className="pl-4">
+                {groupedVersions[songId]
+                  .sort((a, b) => a.version - b.version)
+                  .map((t, j) => (
+                    <li key={j}>{generateLink(t)}</li>
+                  ))}
+              </ul>
+            </details>
+          )}
+        </li>
+      ))}
+    </>
+  ) : (
+    <>
+      {allTabs.map((t, i) => (
+        <li key={i}>{generateLink(t)}</li>
+      ))}
+    </>
+  );
+
   return (
     <>
       <Head>
         <title>Song Directory</title>
       </Head>
-      <div className="max-w-xl mx-auto flex flex-col gap-2 my-8">
-        <div className="mx-x">
-            {searchText.length > 0
-              ? 'Searching for "' + searchText + '" in the directory'
-              : "Directory"}
-          <select
-            className="p-2 rounded-md float-right"
-            name="order"
-            id="order"
-            defaultValue={router.query.order}
-            onChange={(e) => router.push(`/directory/${e.target.value}`)}
-          >
-            <option key="artist" value="artist">
-              By artist
-            </option>
-            <option key="new" value="new">
-              By newest
-            </option>
-            <option key="old" value="old">
-              By oldest
-            </option>
-          </select>
+      <div className="w-fit m-auto wrap">
+        <div className="flex justify-between items-center gap-2 flex-wrap-reverse">
           <div>
-            {Object.keys(multipleVersions).length} songs, {allTabs.length} tabs
+            {Object.keys(groupedVersions).length} songs, {allTabs.length} tabs
           </div>
-          <div className="mb-4">
-            There are {Object.keys(multipleVersions).length} songs,{" "}
-            {filteredTabs.length} tabs
+          <div className="flex gap-2 items-center justify-end">
+            <label className="whitespace-nowrap">
+              <input
+                type="checkbox"
+                checked={collapseVersions}
+                onChange={() => setCollapseVersions((old) => !old)}
+              />{" "}
+              Group by song
+            </label>{" "}
+            <select
+              className="p-2 rounded-md"
+              name="order"
+              id="order"
+              defaultValue={router.query.order}
+              onChange={(e) => router.push(`/directory/${e.target.value}`)}
+            >
+              <option key="artist" value="artist">
+                By artist
+              </option>
+              <option key="new" value="new">
+                By newest
+              </option>
+              <option key="old" value="old">
+                By oldest
+              </option>
+            </select>
           </div>
-          {filteredTabs.map((t, i) => {
-            let s = savedTabs.find(
-              ({ name, artist, type, version }) =>
-                name === t.song.name &&
-                artist === t.song.artist &&
-                type === t.type &&
-                version === t.version
-            );
-            let d = convertToTabLink(t);
-            d.type = t.type;
-            return (
-              <div className="mb-4 font-sm" key={i}>
-                <TabLink tablink={{ ...d, saved: s?.saved }} recent={false} />
-              </div>
-            );
-          })}
+        </div>
+        <hr className="m-2" />
+
+        <div className="mx-8">
+          <ol className=" max-w-xl">{tabList}</ol>
         </div>
       </div>
     </>
